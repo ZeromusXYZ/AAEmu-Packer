cf3d733833d2deb0eea79074d7dcc3e63ba23353
ZeromusXYZ <zeromus.xyz@gmail.com>
2021-04-25 19:39:50 +0200

    -add: calculate MD5 of all files option (#5)
    Also fixed a bug with checking against null-hash for MD5

---
+++ AAPakEditor/ReMD5Dlg.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace AAPakEditor
+{
+    partial class ReMD5Dlg
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.lInfo = new System.Windows.Forms.Label();
+            this.pbRehash = new System.Windows.Forms.ProgressBar();
+            this.bgwRehash = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Enabled = false;
+            this.btnCancel.Location = new System.Drawing.Point(374, 71);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.UseWaitCursor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // lInfo
+            // 
+            this.lInfo.AutoSize = true;
+            this.lInfo.Location = new System.Drawing.Point(12, 51);
+            this.lInfo.Name = "lInfo";
+            this.lInfo.Size = new System.Drawing.Size(59, 13);
+            this.lInfo.TabIndex = 4;
+            this.lInfo.Text = "progress ...";
+            this.lInfo.UseWaitCursor = true;
+            // 
+            // pbRehash
+            // 
+            this.pbRehash.Location = new System.Drawing.Point(12, 12);
+            this.pbRehash.Name = "pbRehash";
+            this.pbRehash.Size = new System.Drawing.Size(437, 23);
+            this.pbRehash.TabIndex = 3;
+            this.pbRehash.UseWaitCursor = true;
+            // 
+            // bgwRehash
+            // 
+            this.bgwRehash.WorkerReportsProgress = true;
+            this.bgwRehash.WorkerSupportsCancellation = true;
+            this.bgwRehash.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgwRehash_DoWork);
+            this.bgwRehash.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bgwRehash_ProgressChanged);
+            this.bgwRehash.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgwRehash_RunWorkerCompleted);
+            // 
+            // ReMD5Dlg
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 106);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.lInfo);
+            this.Controls.Add(this.pbRehash);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ReMD5Dlg";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Recalculate MD5 Hashes";
+            this.Load += new System.EventHandler(this.ReMD5Dlg_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label lInfo;
+        private System.Windows.Forms.ProgressBar pbRehash;
+        private System.ComponentModel.BackgroundWorker bgwRehash;
+    }
+}
---
+++ AAPakEditor/ReMD5Dlg.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Linq;
+
+namespace AAPakEditor
+{
+    public partial class ReMD5Dlg : Form
+    {
+
+        public AAPak pak;
+        public bool allFiles = false;
+        private int toUpdate = 0;
+        private int updated = 0;
+        private string lastFile = string.Empty;
+
+        public ReMD5Dlg()
+        {
+            InitializeComponent();
+        }
+
+        private void ReMD5Dlg_Load(object sender, EventArgs e)
+        {
+            if (allFiles)
+                Text += " (force all)";
+            bgwRehash.RunWorkerAsync();
+            btnCancel.Enabled = true;
+            btnCancel.Text = "Cancel";
+            lInfo.Text = "Scanning " + pak.files.Count.ToString() + " files ...";
+            toUpdate = 0;
+
+            // Count files
+            foreach (var pfi in pak.files)
+            {
+                if (bgwRehash.CancellationPending)
+                    break;
+
+                if (allFiles || pfi.md5.SequenceEqual(AAPakFileHeader.nullHash))
+                    toUpdate++;
+            }
+
+            lInfo.Text = "Updating " + toUpdate.ToString() + " files ...";
+        }
+
+        private void bgwRehash_DoWork(object sender, DoWorkEventArgs e)
+        {
+            updated = 0;
+            System.Threading.Thread.Sleep(1000);
+
+            // Actually update
+            foreach (var pfi in pak.files)
+            {
+                if (bgwRehash.CancellationPending)
+                    break;
+
+                if (allFiles || pfi.md5.SequenceEqual(AAPakFileHeader.nullHash))
+                {
+                    updated++;
+                    pak.UpdateMD5(pfi);
+
+                    var p = updated * 100 / toUpdate;
+                    if ((updated < toUpdate) && (p >= 100))
+                        p = 99;
+                    lastFile = pfi.name;
+                    bgwRehash.ReportProgress((int)p);
+                }
+            }
+
+            bgwRehash.ReportProgress(100);
+            System.Threading.Thread.Sleep(1000);
+
+            if (bgwRehash.CancellationPending)
+            {
+                MessageBox.Show("Remaining rehash cancelled !");
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            bgwRehash.CancelAsync();
+            btnCancel.Enabled = false;
+        }
+
+        private void bgwRehash_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if ((e.ProgressPercentage < 100) && (lastFile != string.Empty))
+                lInfo.Text = updated.ToString() + " / " + toUpdate.ToString() + "  " + lastFile;
+            else
+                lInfo.Text = "Updated " + updated.ToString() + " / " + toUpdate.ToString() + " files !";
+            pbRehash.Minimum = 0;
+            pbRehash.Maximum = toUpdate;
+            pbRehash.Value = updated;
+            if (e.ProgressPercentage >= 100)
+            {
+                btnCancel.Enabled = false;
+            }
+        }
+
+        private void bgwRehash_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (updated > 0)
+                MessageBox.Show("Update completed !");
+            else
+                MessageBox.Show("Everything was already up to date !");
+            Close();
+        }
+    }
+}
